af1dc180801fc6ed96b4ae4c56f506711b83b4ea
Christine Jeon <csjeon28@gmail.com>
2021-07-27 13:06:24 -0400

    added classnames for css later

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import NewUser from './NewUser'
 
 const Home = () => {
 
     return (
         <div>
-            <header className='home'>
-                <h1 className='home-header'>My Wish Board</h1>
+            <header className='home-container'>
+                <h1>My Wish Board</h1>
+                <div className='home-flex-row'>
+                    <div className='home-flex-large'>
+                        <h2>New User</h2>
+                        <NewUser />
+                    </div>
+                </div>
             </header>
         </div>
     )
