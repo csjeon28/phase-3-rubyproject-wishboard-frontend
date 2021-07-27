a818860e883882e755b354da466c9fab1d4e0b64
Christine Jeon <csjeon28@gmail.com>
2021-07-27 02:58:56 -0400

    added Home page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+
+    return (
+        <div>
+            <header className='home'>
+                <h1 className='home-header'>My Wish Board</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Home
