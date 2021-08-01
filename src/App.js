bde2cc67a0f0f08de5fe70d0f69697f04b1d0dbb
Christine Jeon <csjeon28@gmail.com>
2021-08-01 16:33:17 -0400

    took out commented out codes

--- src/App.js
+++ src/App.js
@@ -4,30 +4,21 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Home from '../src/components/Home'
 import LogUser from '../src/components/LogUser'
 import Board from '../src/components/Board'
-import Wishes from '../src/components/Wishes'
 
 const App = () => {
   const [username, setUsername] = useState('')
-  // const [boardadd, setBoardadd] = useState('')
   const [user, setUser] = useState({})
-  const [boards, setBoards] = useState([])
 
   useEffect(() => {
     const fetchUsers = () => {
       fetch(`http://localhost:9292/users/search?q=${username}`)
         .then(resp => resp.json())
-        .then(data => {
-          setUser(data.user)
-          setBoards(data.userBoards)
+        .then(resp => {
+          setUser(resp.data.user)
         })
     }
     fetchUsers()
   }, [username])
-
-  // const updateBoards = (id) => {
-  //   const updatedBoards = boards.filter(b => b.id !== id)
-  //   setBoards(updatedBoards)
-  // }
 
   return (
     <div className='app'>
@@ -41,15 +32,10 @@ const App = () => {
             <Route exact path='/' component={Home} />
             <Route exact path='/home' component={Home} />
             <Route exact path='/loguser'>
-              <LogUser setBoards={setBoards} setUsername={setUsername} user={user} />
+              <LogUser setUsername={setUsername} user={user} />
             </Route>
             <Route exact path='/boards'>
-              <Board boards={boards} username={username} />
-              {/* <Board updateBoards={updateBoards} boards={boards} username={username} /> */}
-              {/* <Board setBoardadd={setBoardadd} boardadd={boardadd} boards={boards} username={username} /> */}
-            </Route>
-            <Route exact path='/wishes'>
-              <Wishes />
+              <Board username={username} />
             </Route>
           </Switch>
         </div>
