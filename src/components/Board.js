5ca6ff9acf6ff1323d39b3e7b979c7907ef6c6dd
Christine Jeon <csjeon28@gmail.com>
2021-08-04 13:45:39 -0400

    able to display only boards of signed in user

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import BoardAdd from './BoardAdd'
 
-const Board = ({ username }) => {
+const Board = ({ setBoards, boards, username }) => {
     const { push } = useHistory()
 
     const [boardList, setBoardList] = useState([])
 
-    useEffect(() => {
-        fetch('http://localhost:9292/boards/')
-            .then(resp => resp.json())
-            .then(resp => {
-                setBoardList(resp.data.board)
-            })
-    }, [])
+    // useEffect(() => {
+    //     fetch('http://localhost:9292/boards/')
+    //         .then(resp => resp.json())
+    //         .then(resp => {
+    //             setBoardList(resp.data.board)
+    //             console.log(resp.data.board)
+    //         })
+    // }, [])
+
+    const handleDelete = () => {
+        const deleteBoardReq = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        fetch('http://localhost:9292/boards/' + boards.id, deleteBoardReq)
+        const newBoards = boardList.filter(b => {
+            return b.id !== boards.id
+        })
+        setBoardList([...boards, newBoards])
+    }
 
     return (
         <div className='board'>
@@ -23,13 +38,15 @@ const Board = ({ username }) => {
                 <button className='board-logout-button' onClick={() => push('/home')}>Log Out</button>
             </div>
             <div>
-                <BoardAdd />
-                {boardList.map((board, index) => {
+                <BoardAdd setBoards={setBoards} username={username} boards={boards} />
+                {boards.map((board, index) => {
                     return (
                         <div
                             className='board-tile'
+                            id={board.id}
                             key={index}>
                             {board.name}
+                            <button onClick={handleDelete}>X</button>
                         </div>
                     )
                 })}
